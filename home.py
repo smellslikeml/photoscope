de6224b3420c306583d30a5cdbd3b4c5f7432597
mayorquinmachines <smayorquin@berkeley.edu>
2018-10-23 16:11:50 -0700

    changed image directory

--- home.py
+++ home.py
@@ -6,11 +6,23 @@ import time
 from glob import glob
 from functools import partial
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, send_from_directory
 
 app=Flask(__name__)
 
-data_dir = '/path/to/images'
+def color_hist(bins, img_pth):
+    img = cv2.imread(img_pth, 0)
+    hist = cv2.calcHist([img], [0], None, [bins], [0, 256])
+    return hist / (sum(hist) + 1e-10)
+
+def dist_calc(query_img, ref_img):
+    return cv2.compareHist(query_img, ref_img, cv2.HISTCMP_CHISQR)
+
+def transform_query(img_arr):
+    hist = cv2.calcHist([img_arr], [0], None, [bins], [0, 256])
+    return hist / (sum(hist) + 1e-10)
+
+data_dir = '/path/to/images/'
 bins = 128
 num_results = 50
 
@@ -26,17 +38,6 @@ elapsed = end - start
 elapsed_fmt = time.strftime("%H:%M:%S", time.gmtime(elapsed))
 print('Finished feature calculation in {}, beginning search...'.format(elapsed_fmt))
 
-def color_hist(bins, img_pth):
-    img = cv2.imread(img_pth, 0)
-    hist = cv2.calcHist([img], [0], None, [bins], [0, 256])
-    return hist / (sum(hist) + 1e-10)
-
-def dist_calc(query_img, ref_img):
-    return cv2.compareHist(query_img, ref_img, cv2.HISTCMP_CHISQR)
-
-def transform_query(img_arr):
-    hist = cv2.calcHist([img_arr], [0], None, [bins], [0, 256])
-    return hist / (sum(hist) + 1e-10)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -50,9 +51,13 @@ def home():
 	query_dist = partial(dist_calc, query_hist)
 	result_dist = list(map(query_dist, corpus_features))
 	result_paths = np.argsort(result_dist)[:num_results]
-	result_paths = [os.path.join(data_dir, img_lst[pth]) for pth in result_paths]
+	result_paths = [img_lst[pth].split('/')[-1] for pth in result_paths]
 	return render_template("results.html", result_paths=result_paths)
     return render_template("index.html")
+
+@app.route('/image/<path:filename>')
+def get_image(filename):
+    return send_from_directory(data_dir, filename)
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
